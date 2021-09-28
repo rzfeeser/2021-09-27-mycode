02386bbd5be775448b06d82ae3ffc8a0f4dd287a
RZFeeser <rzfeeser@users.noreply.github.com>
2021-09-28 14:04:15 +0000

    start of Tuesday

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# collecting a value for "user_input" from the CLI
+user_input = input("Please enter an IPv4 IP address:")
+
+# using print function to display our string plus user input
+print("You told me the IPv4 address is:", user_input)
+
